ddbfba579a984b5036e1839c97341f33915cb1a6
Mohammadhassan Tohidi <tohidihassan9@gmail.com>
2024-03-23 16:55:01 +0330

    Add project files.

---
+++ Exponentiation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exponentiation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter base: ");
+            int baseValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter exponent: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Result: {IntegerPower(baseValue, exponent)}");
+        }
+
+        static int IntegerPower(int baseValue, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+                result *= baseValue;
+
+            return result;
+        }
+    }
+}
